9724444a8bc5ea60840b23a180f33f109dd9450a
kip <cyruskiprop254@gmail.com>
2020-04-22 11:50:47 +0300

    validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
-  has_secure_password
+  before_save { self.email = email.downcase }
 
   has_many :posts, dependent: :destroy
+
+  validates :fname, presence: true
+  validates :lname, presence: true
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
